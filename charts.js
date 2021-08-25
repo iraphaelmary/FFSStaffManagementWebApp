3500bbc281064f7cfe0e19aa950a00b9f2c31c58
iraphaelmary <47445382+iraphaelmary@users.noreply.github.com>
2021-08-25 19:57:16 +0100

    Add files via upload

---
+++ charts.js
@@ -0,0 +1,120 @@
+
+// let label_1 = ['ASF II', 'ASF I', 'DSF', 'SF', 'CSF', 'ACF', 'DCF', 'CF', 'ACG'];
+// let myChart1 = document.getElementById('chart1').getContext('2d');
+// let mychart = new Chart(myChart1, {
+//     type: 'radar',
+//     data: {
+//         labels: label_1,
+//         datasets: [{
+//                 label: 'Male',
+//                     fill: true,
+//                     backgroundColor: '#d62828',
+//                     borderColor: '#f77f00',
+//                     PointBorderColor: '#fcbf49',
+//                     data: [324, 111, 288, 122, 444, 654, 188, 12, 8]
+//             },
+//             {
+//                 label: 'female',
+//                     fill: true,
+//                     backgroundColor: '#ccc',
+//                     borderColor: '#333',
+//                     PointBorderColor: '#777',
+//                     data: [124, 91, 88, 32, 324, 54, 18, 211, 5]
+//         }]
+//     },
+//     options: {
+//         title: {
+//             title: "Gender Rank Distribution",
+//             display: true
+//         }
+//     }
+// });
+
+
+// let label_1 = ['Commissioned', 'Non-Commissioned'];
+// let data_1 = [5471, 2181];
+// let colors_1 = ['#49a9ea', '#36caab'];
+// let gender = document.getElementById('dou').getContext('2d');
+
+// let genderData = new Chart(gender, {
+//     type: 'pie',
+//     data: {
+//         labels: label_1,
+//         datasets: [{
+//             data: data_1,
+//             backgroundColor: colors_1
+//         }]
+//     },
+//     options: {
+//         title: {
+//             title: "Gender Distribution",
+//             display: true
+//         }
+//     }
+// })
+
+// let label_2 = ['Male', 'Female'];
+// let data_2 = [5271, 2281];
+// let colors_2 = ['#49a9ea', '#36caab'];
+// let bars = document.getElementById('bar').getContext('2d');
+
+// let barData = new Chart(bars, {
+//     type: 'bar',
+//     data: {
+//         labels: label_2,
+//         datasets: [{
+//             data: data_2,
+//             backgroundColor: colors_2
+//         }]
+//     },
+//     options: {
+//         title: {
+//             title: "Gender Distribution",
+//             display: true
+//         }
+//     }
+// })
+
+//         let label = ['France', 'Nigeria', 'Israel'];
+//         let data_ = [100, 160, 80];
+//         let colors = ['#00ffff', '#ccc', '#ff00ff'];
+//         let mychart = document.getElementById('chart').getContext('2d');
+
+//         let chart = new Chart(mychart, {
+//             type: 'pie',
+//             data: {
+//                 labels: label,
+//                 datasets: [{
+//                     data: data_,
+//                     backgroundColor: colors
+//                 }]
+//             },
+//             options: {
+//                 title: {
+//                     title: 'Countries Population',
+//                     display: true
+//                 }
+//             }
+//         })
+
+// let gender = document.getElementById('dou').getContext('2d');
+// let label_1 = ['Male', 'Female'];
+// let data_1 = ['5471', '2181'];
+// let colors_1 = ['#49a9ea', '#36caab'];
+
+// let genderData = new Chart(gender, {
+//     type: 'pie',
+//     data: {
+//         labels: label_1,
+//         datasets: [{
+//             data: data_1,
+//             backgroundColor: colors_1
+//         }]
+//     },
+//     options: {
+//         title: {
+//             title: "Gender Distribution",
+//             display: true
+//         }
+//     }
+// })
